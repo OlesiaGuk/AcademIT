4e8f3e782b7c36e0be79be4fb9d836f8b0c8d79c
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-07-22 07:57:38 +0700

    Реализованы все нестатические методы

--- Vector/src/guk/vector/Vector.java
+++ Vector/src/guk/vector/Vector.java
@@ -1,42 +1,28 @@
 package guk.vector;
 
+import java.util.Arrays;
+
 public class Vector {
-    private int n;
     private double[] componentsArray;
 
     public Vector(int n) {
         if (n <= 0)
             throw new IllegalArgumentException("Размерность вектора должны быть > 0");
-        this.n = n;
         componentsArray = new double[n];
     }
 
     public Vector(Vector vector) {
-        this(vector.n, vector.componentsArray);
+        this(vector.componentsArray);
     }
 
     public Vector(double[] array) {
-        n = array.length;
-        componentsArray = new double[n];
-        for (int i = 0; i < n; i++) {
-            componentsArray[i] = array[i];
-        }
+        componentsArray = new double[array.length];
+        System.arraycopy(array, 0, componentsArray, 0, array.length);
     }
 
     public Vector(int n, double[] array) {
-        this.n = n;
         componentsArray = new double[n];
-        for (int i = 0; i < array.length; i++) {
-            componentsArray[i] = array[i];
-        }
-    }
-
-    public int getN() {
-        return n;
-    }
-
-    public void setN(int n) {
-        this.n = n;
+        System.arraycopy(array, 0, componentsArray, 0, array.length);
     }
 
     public double[] getComponentsArray() {
@@ -47,9 +33,8 @@ public class Vector {
         this.componentsArray = componentsArray;
     }
 
-    //Метод getSize() для получения размерности вектора - чем отличается от геттера для поля n???
-    public int getSize(){
-        return n;
+    public int getSize() {
+        return componentsArray.length;
     }
 
     @Override
@@ -57,12 +42,108 @@ public class Vector {
         StringBuilder s = new StringBuilder();
         int i = 0;
         s.append("{");
-        while (i < n - 1) {
+        while (i < componentsArray.length - 1) {
             s.append(componentsArray[i]).append(", ");
             i++;
         }
         s.append(componentsArray[i]).append("}");
-        String line = s.toString();
-        return line;
+        return s.toString();
     }
+
+    public Vector getVectorsSum(Vector vector) {
+        int minArraySize = Math.min(this.getSize(), vector.getSize());
+        int maxArraySize = Math.max(this.getSize(), vector.getSize());
+
+        double[] summaryArray = new double[maxArraySize];
+        int i = 0;
+        while (i < minArraySize) {
+            summaryArray[i] = this.componentsArray[i] + vector.componentsArray[i];
+            i++;
+        }
+        if (this.getSize() == maxArraySize) {
+            System.arraycopy(this.componentsArray, i, summaryArray, i, summaryArray.length - i);
+        } else {
+            System.arraycopy(vector.componentsArray, i, summaryArray, i, summaryArray.length - i);
+        }
+        return new Vector(summaryArray);
+    }
+
+    public void getVectorScalarMultiply(double scalar) {
+        for (int i = 0; i < this.getSize(); i++) {
+            this.componentsArray[i] = this.componentsArray[i] * scalar;
+        }
+    }
+
+    public void getVectorReversal() {
+        this.getVectorScalarMultiply(-1);
+    }
+
+    public Vector getVectorsDifference(Vector vector) {
+        vector.getVectorReversal();
+        return getVectorsSum(vector);
+    }
+
+    public double getVectorLength() {
+        double squaresSum = 0;
+        for (int i = 0; i < this.getSize(); i++) {
+            squaresSum += Math.pow(this.getComponentsArray()[i], 2);
+        }
+        return Math.sqrt(squaresSum);
+    }
+
+    public double getVectorComponentByIndex(int index) {
+        return componentsArray[index];
+    }
+
+    public Vector setVectorComponentByIndex(double newComponent, int index) {
+        if (index > componentsArray.length) {
+            throw new IllegalArgumentException("Введенный индекс превышает размерность вектора");
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс должен быть >= 0");
+        }
+        double[] newComponentsArray = new double[componentsArray.length + 1];
+        int i = 0;
+        while (i < index) {
+            newComponentsArray[i] = componentsArray[i];
+            i++;
+        }
+        int j = i;
+        newComponentsArray[j] = newComponent;
+        j++;
+        while (j < newComponentsArray.length) {
+            newComponentsArray[j] = componentsArray[i];
+            i++;
+            j++;
+        }
+        return new Vector(newComponentsArray);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+        Vector vector = (Vector) o;
+        if (componentsArray.length != vector.componentsArray.length) {
+            return false;
+        }
+        for (int i = 0; i < componentsArray.length - 1; i++) {
+            if (componentsArray[i] != vector.componentsArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        return prime * hash + Arrays.hashCode(componentsArray);
+    }
+
 }
--- Vector/src/guk/vectorMain/Main.java
+++ Vector/src/guk/vectorMain/Main.java
@@ -5,10 +5,27 @@ import guk.vector.Vector;
 public class Main {
     public static void main(String[] args) {
 
-        double[] array = {1,2,3};
-        Vector vector = new Vector( array);
-        System.out.println(vector);
+        Vector emptyVector = new Vector(3);
+        System.out.println(emptyVector);
 
-        System.out.println(vector.getSize());
+        double[] array = {-3, 4};
+        Vector vector = new Vector(5, array);
+        System.out.println("vector = " + vector);
+
+        Vector vectorCopy = new Vector(vector);
+        System.out.println("Копия вектора vector: " + vectorCopy);
+
+        int index = 1;
+        System.out.printf("Компонента вектора по индексу %d = %.2f %n", index, vector.getVectorComponentByIndex(index));
+
+        Vector vector2 = vector.setVectorComponentByIndex(10, 0);
+        System.out.println("vector2 = " + vector2);
+
+        System.out.println("Разность векторов vector и vector2 = " + vector.getVectorsDifference(vector2));
+
+        Vector vector3 = new Vector(5, array);
+        System.out.println(vector.equals(vector3));
+
+
     }
 }
