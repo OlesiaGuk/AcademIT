a424a1f27469c0f04e388b8647c222281f2a24f4
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-07-25 14:29:18 +0700

    Реализованы методы получения размеров матрицы, получения и задания вектора-строки по индексу

--- Matrix/src/guk/matrix/Matrix.java
+++ Matrix/src/guk/matrix/Matrix.java
@@ -15,15 +15,15 @@ public class Matrix {
     public Matrix(double[][] array) {
         matrixElementsArray = new Vector[array.length];
         for (int i = 0; i < array.length; i++) {
-            matrixElementsArray[i] = new Vector(array[i]);
+            matrixElementsArray[i] = new Vector(findMaxSize(array), array[i]);
         }
     }
 
-    public Matrix(Vector[] vectorsArray) {
+    public Matrix(Vector[] vectorsArray) { //todo: добить нулями
         matrixElementsArray = vectorsArray;
     }
 
-    public Matrix(Matrix matrix){
+    public Matrix(Matrix matrix) {
         this(matrix.matrixElementsArray);
     }
 
@@ -33,6 +33,22 @@ public class Matrix {
 
     public void setMatrixElementsArray(Vector[] matrixElementsArray) {
         this.matrixElementsArray = matrixElementsArray;
+    }
+
+    private int findMaxVectorSize() {//todo: метод неоптимальный, переделать?
+        int maxVectorIndex = 0;
+        for (int i = 1; i < matrixElementsArray.length; i++) {
+            maxVectorIndex = (matrixElementsArray[i].getSize() > matrixElementsArray[maxVectorIndex].getSize()) ? i : maxVectorIndex;
+        }
+        return matrixElementsArray[maxVectorIndex].getSize();
+    }
+
+    private static int findMaxSize(double[][] array) {//todo: метод неоптимальный, переделать?
+        int maxSizeElementIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            maxSizeElementIndex = (array[i].length > array[maxSizeElementIndex].length) ? i : maxSizeElementIndex;
+        }
+        return array[maxSizeElementIndex].length;
     }
 
     @Override
@@ -55,5 +71,43 @@ public class Matrix {
         }
         s.append("}");
         return s.toString();
+    }
+
+    public int[] getMatrixSize() {
+        return new int[]{matrixElementsArray.length, findMaxVectorSize()};
+    }
+
+    public Vector getVectorByIndex(int index) {
+        if (index >= matrixElementsArray.length) {
+            throw new IllegalArgumentException("Индекс должен быть < " + matrixElementsArray.length);
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс должен быть >= 0");
+        }
+        return matrixElementsArray[index];
+    }
+
+    public Matrix setVectorByIndex(int index, Vector newVector) {
+        if (index >= matrixElementsArray.length + 1) {
+            throw new IllegalArgumentException("Индекс должен быть < " + matrixElementsArray.length);
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс должен быть >= 0");
+        }
+        Vector[] newMatrixElementsArray = new Vector[matrixElementsArray.length + 1];
+        int i = 0;
+        while (i < index) {
+            newMatrixElementsArray[i] = matrixElementsArray[i];
+            i++;
+        }
+        int j = i;
+        newMatrixElementsArray[j] = newVector;
+        j++;
+        while (j < newMatrixElementsArray.length) {
+            newMatrixElementsArray[j] = matrixElementsArray[i];
+            i++;
+            j++;
+        }
+        return new Matrix(newMatrixElementsArray);
     }
 }
--- Matrix/src/guk/matrixMain/Main.java
+++ Matrix/src/guk/matrixMain/Main.java
@@ -3,12 +3,14 @@ package guk.matrixMain;
 import guk.matrix.Matrix;
 import guk.vector.Vector;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(2, 3);
         System.out.println(matrix);
 
-        double[][] array = {{1, 2, 3}, {4, 5, 6}};
+        double[][] array = {{1, 2, 3}, {4, 5, 6, 7}};
         Matrix matrix2 = new Matrix(array);
         System.out.println(matrix2);
 
@@ -20,5 +22,12 @@ public class Main {
 
         Matrix matrixCopy = new Matrix(matrix3);
         System.out.println(matrixCopy);
+
+        System.out.println(Arrays.toString(matrix2.getMatrixSize()));
+
+        System.out.println(matrix3.getVectorByIndex(1));
+
+        Vector newVector = new Vector(2, new double[]{20, 21});
+        System.out.println(matrixCopy.setVectorByIndex(0, newVector));
     }
 }
