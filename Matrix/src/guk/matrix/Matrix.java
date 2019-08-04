1608783694c572c480ab30177052899e2175a7fc
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-08-04 10:32:38 +0700

    скорректированы названия методов, конструктор из массива векторов-строк, в методе toString while заменен на for, исправлен setVectorByIndex, реализован метод получения вектора-стобца по индексу

--- Matrix/src/guk/matrix/Matrix.java
+++ Matrix/src/guk/matrix/Matrix.java
@@ -3,111 +3,115 @@ package guk.matrix;
 import guk.vector.Vector;
 
 public class Matrix {
-    private Vector[] matrixElementsArray;
+    private Vector[] matrixElements;
 
     public Matrix(int n, int m) {
-        matrixElementsArray = new Vector[n];
+        matrixElements = new Vector[n];
         for (int i = 0; i < n; i++) {
-            matrixElementsArray[i] = new Vector(m);
+            matrixElements[i] = new Vector(m);
         }
     }
 
     public Matrix(double[][] array) {
-        matrixElementsArray = new Vector[array.length];
+        matrixElements = new Vector[array.length];
+        int maxSize = findMaxNestedArraySize(array);
         for (int i = 0; i < array.length; i++) {
-            matrixElementsArray[i] = new Vector(findMaxSize(array), array[i]);
+            matrixElements[i] = new Vector(maxSize, array[i]);
         }
     }
 
-    public Matrix(Vector[] vectorsArray) { //todo: добить нулями
-        matrixElementsArray = vectorsArray;
+    public Matrix(Vector[] vectorsArray) {
+        matrixElements = new Vector[vectorsArray.length];
+        int maxSize = findMaxVectorSize(vectorsArray);
+        for (int i = 0; i < vectorsArray.length; i++) {
+            matrixElements[i] = new Vector(maxSize);
+            for (int j = 0; j < vectorsArray[i].getSize(); j++) {
+                matrixElements[i].setComponentByIndex(vectorsArray[i].getComponentByIndex(j), j);
+            }
+        }
     }
 
     public Matrix(Matrix matrix) {
-        this(matrix.matrixElementsArray);
+        this(matrix.matrixElements);
     }
 
-    public Vector[] getMatrixElementsArray() {
-        return matrixElementsArray;
-    }
-
-    public void setMatrixElementsArray(Vector[] matrixElementsArray) {
-        this.matrixElementsArray = matrixElementsArray;
-    }
-
-    private int findMaxVectorSize() {//todo: метод неоптимальный, переделать?
-        int maxVectorIndex = 0;
-        for (int i = 1; i < matrixElementsArray.length; i++) {
-            maxVectorIndex = (matrixElementsArray[i].getSize() > matrixElementsArray[maxVectorIndex].getSize()) ? i : maxVectorIndex;
+    private static int findMaxVectorSize(Vector[] array) {
+        int maxVectorSize = 0;
+        for (Vector e : array) {
+            maxVectorSize = (e.getSize() > maxVectorSize) ? e.getSize() : maxVectorSize;
         }
-        return matrixElementsArray[maxVectorIndex].getSize();
+        return maxVectorSize;
     }
 
-    private static int findMaxSize(double[][] array) {//todo: метод неоптимальный, переделать?
-        int maxSizeElementIndex = 0;
-        for (int i = 1; i < array.length; i++) {
-            maxSizeElementIndex = (array[i].length > array[maxSizeElementIndex].length) ? i : maxSizeElementIndex;
+    private static int findMaxNestedArraySize(double[][] array) {
+        int maxSize = 0;
+        for (double[] e : array) {
+            maxSize = (e.length > maxSize) ? e.length : maxSize;
         }
-        return array[maxSizeElementIndex].length;
+        return maxSize;
     }
 
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
         s.append("{");
-        int i = 0;
-        while (i < matrixElementsArray.length) {
+        for (int i = 0; i < matrixElements.length; i++) {
             s.append("{");
-            int j = 0;
-            while (j < matrixElementsArray[i].getSize() - 1) {
-                s.append(matrixElementsArray[i].getComponentByIndex(j)).append(", ");
-                j++;
+
+            for (int j = 0; j < matrixElements[i].getSize() - 1; j++) {
+                s.append(matrixElements[i].getComponentByIndex(j)).append(", ");
             }
-            s.append(matrixElementsArray[i].getComponentByIndex(j)).append("}");
-            if (i != matrixElementsArray.length - 1) {
+            s.append(matrixElements[i].getComponentByIndex(matrixElements[i].getSize() - 1)).append("}");
+
+            if (i != matrixElements.length - 1) {
                 s.append(", ");
             }
-            i++;
         }
         s.append("}");
         return s.toString();
     }
+//============================================================================================
 
-    public int[] getMatrixSize() {
-        return new int[]{matrixElementsArray.length, findMaxVectorSize()};
+    public int[] getSize() { //todo: а если матрица непрямоугольная?(например, после вставки вектора)
+        return new int[]{matrixElements.length, matrixElements[0].getSize()};
     }
 
     public Vector getVectorByIndex(int index) {
-        if (index >= matrixElementsArray.length) {
-            throw new IllegalArgumentException("Индекс должен быть < " + matrixElementsArray.length);
+        if (index >= matrixElements.length) {
+            throw new IllegalArgumentException("Индекс должен быть < " + matrixElements.length);
         }
         if (index < 0) {
             throw new IllegalArgumentException("Индекс должен быть >= 0");
         }
-        return matrixElementsArray[index];
+
+        return matrixElements[index];
     }
 
-    public Matrix setVectorByIndex(int index, Vector newVector) {
-        if (index >= matrixElementsArray.length + 1) {
-            throw new IllegalArgumentException("Индекс должен быть < " + matrixElementsArray.length);
+    public void setVectorByIndex(int index, Vector newVector) {
+        if (index >= matrixElements.length) {
+            throw new IllegalArgumentException("Индекс должен быть < " + matrixElements.length);
         }
         if (index < 0) {
             throw new IllegalArgumentException("Индекс должен быть >= 0");
         }
-        Vector[] newMatrixElementsArray = new Vector[matrixElementsArray.length + 1];
-        int i = 0;
-        while (i < index) {
-            newMatrixElementsArray[i] = matrixElementsArray[i];
-            i++;
+
+        matrixElements[index] = newVector;
+    }
+
+
+    public Vector getVectorColumnByIndex(int index) { //todo: а если матрица непрямоугольная?(например, после вставки вектора)
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс должен быть >= 0");
         }
-        int j = i;
-        newMatrixElementsArray[j] = newVector;
-        j++;
-        while (j < newMatrixElementsArray.length) {
-            newMatrixElementsArray[j] = matrixElementsArray[i];
-            i++;
-            j++;
+        if (index > getSize()[1] - 1) {
+            throw new ArrayIndexOutOfBoundsException("Введенный индекс превышает размерность вектора");
         }
-        return new Matrix(newMatrixElementsArray);
+
+        double[] array = new double[matrixElements.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = matrixElements[i].getComponentByIndex(index);
+        }
+        return new Vector(array);
     }
 }
+
--- Matrix/src/guk/matrixMain/Main.java
+++ Matrix/src/guk/matrixMain/Main.java
@@ -8,26 +8,29 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(2, 3);
-        System.out.println(matrix);
+        System.out.println("Матрица нулей matrix размера " + Arrays.toString(matrix.getSize()) + " = " + matrix);
 
-        double[][] array = {{1, 2, 3}, {4, 5, 6, 7}};
+        double[][] array = new double[][]{{1, 2, 3}, {4, 5, 6, 7}};
         Matrix matrix2 = new Matrix(array);
-        System.out.println(matrix2);
+        System.out.println("Матрица matrix2 = " + matrix2);
 
-        double[] array2 = {7, 8};
-        double[] array3 = {9, 10, 11};
-        Vector[] vector = {new Vector(array2), new Vector(array3)};
+        Matrix matrix2Copy = new Matrix(matrix2);
+        System.out.println("Копия матрицы matrix2 = " + matrix2Copy);
+
+        System.out.println();
+        Vector[] vector = new Vector[]{new Vector(new double[]{7, 8}), new Vector(new double[]{9, 10, 11})};
         Matrix matrix3 = new Matrix(vector);
-        System.out.println(matrix3);
+        System.out.println("Матрица matrix3 = " + matrix3);
 
-        Matrix matrixCopy = new Matrix(matrix3);
-        System.out.println(matrixCopy);
+        int index = 1;
+        System.out.println("Вектор под индексом " + index + " матрицы matrix3 = " + matrix3.getVectorByIndex(index));
 
-        System.out.println(Arrays.toString(matrix2.getMatrixSize()));
+        int index3 = 2;
+        System.out.println(matrix3.getVectorColumnByIndex(index3));
 
-        System.out.println(matrix3.getVectorByIndex(1));
+        int index2 = 0;
+        matrix2Copy.setVectorByIndex(index2, new Vector(2, new double[]{20, 21}));
+        System.out.println("Установка вектора-строки {20, 21} по индексу " + index2 + " в матрицу matrix2Copy = " + matrix2Copy);
 
-        Vector newVector = new Vector(2, new double[]{20, 21});
-        System.out.println(matrixCopy.setVectorByIndex(0, newVector));
     }
 }
