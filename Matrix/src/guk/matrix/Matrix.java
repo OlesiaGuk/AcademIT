30405ee42e0e801192545a8d1971ad086deb07c7
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-08-05 14:45:19 +0700

    Реализованы методы: умножение на скаляр, нестатические и статические сложение и вычитание матриц

--- Matrix/src/guk/matrix/Matrix.java
+++ Matrix/src/guk/matrix/Matrix.java
@@ -38,7 +38,7 @@ public class Matrix {
     private static int findMaxVectorSize(Vector[] array) {
         int maxVectorSize = 0;
         for (Vector e : array) {
-            maxVectorSize = (e.getSize() > maxVectorSize) ? e.getSize() : maxVectorSize;
+            maxVectorSize = Math.max(e.getSize(), maxVectorSize);
         }
         return maxVectorSize;
     }
@@ -46,7 +46,7 @@ public class Matrix {
     private static int findMaxNestedArraySize(double[][] array) {
         int maxSize = 0;
         for (double[] e : array) {
-            maxSize = (e.length > maxSize) ? e.length : maxSize;
+            maxSize = Math.max(e.length, maxSize);
         }
         return maxSize;
     }
@@ -70,18 +70,17 @@ public class Matrix {
         s.append("}");
         return s.toString();
     }
-//============================================================================================
 
-    public int[] getSize() { //todo: а если матрица непрямоугольная?(например, после вставки вектора)
+    public int[] getSize() {
         return new int[]{matrixElements.length, matrixElements[0].getSize()};
     }
 
     public Vector getVectorByIndex(int index) {
         if (index >= matrixElements.length) {
-            throw new IllegalArgumentException("Индекс должен быть < " + matrixElements.length);
+            throw new IndexOutOfBoundsException("Индекс должен быть < " + matrixElements.length);
         }
         if (index < 0) {
-            throw new IllegalArgumentException("Индекс должен быть >= 0");
+            throw new IndexOutOfBoundsException("Индекс должен быть >= 0");
         }
 
         return matrixElements[index];
@@ -89,22 +88,24 @@ public class Matrix {
 
     public void setVectorByIndex(int index, Vector newVector) {
         if (index >= matrixElements.length) {
-            throw new IllegalArgumentException("Индекс должен быть < " + matrixElements.length);
+            throw new IndexOutOfBoundsException("Индекс должен быть < " + matrixElements.length);
         }
         if (index < 0) {
-            throw new IllegalArgumentException("Индекс должен быть >= 0");
+            throw new IndexOutOfBoundsException("Индекс должен быть >= 0");
+        }
+        if (newVector.getSize() != getSize()[1]) {
+            throw new IllegalArgumentException("Размерность переданного вектора не соответствует размерности векторов в матрице");
         }
 
         matrixElements[index] = newVector;
     }
 
-
-    public Vector getVectorColumnByIndex(int index) { //todo: а если матрица непрямоугольная?(например, после вставки вектора)
+    public Vector getVectorColumnByIndex(int index) {
         if (index < 0) {
-            throw new IllegalArgumentException("Индекс должен быть >= 0");
+            throw new IndexOutOfBoundsException("Индекс должен быть >= 0");
         }
         if (index > getSize()[1] - 1) {
-            throw new ArrayIndexOutOfBoundsException("Введенный индекс превышает размерность вектора");
+            throw new IndexOutOfBoundsException("Введенный индекс превышает размерность вектора");
         }
 
         double[] array = new double[matrixElements.length];
@@ -113,5 +114,43 @@ public class Matrix {
         }
         return new Vector(array);
     }
+
+    public void multiplyByScalar(double scalar) {
+        for (int i = 0; i < getSize()[0]; i++) {
+            matrixElements[i].multiplyByScalar(scalar);
+        }
+    }
+
+    public void add(Matrix matrix) {
+        if (getSize()[0] != matrix.getSize()[0] || getSize()[1] != matrix.getSize()[1]) {
+            throw new IllegalArgumentException("Матрицы не совпадают по размеру");
+        }
+        for (int i = 0; i < getSize()[0]; i++) {
+            matrixElements[i].add(matrix.matrixElements[i]);
+        }
+    }
+
+    public void subtract(Matrix matrix) {
+        if (getSize()[0] != matrix.getSize()[0] || getSize()[1] != matrix.getSize()[1]) {
+            throw new IllegalArgumentException("Матрицы не совпадают по размеру");
+        }
+        for (int i = 0; i < getSize()[0]; i++) {
+            matrixElements[i].subtract(matrix.matrixElements[i]);
+        }
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        Matrix newMatrix = new Matrix(matrix1);
+        newMatrix.add(matrix2);
+        return newMatrix;
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        Matrix newMatrix = new Matrix(matrix1);
+        newMatrix.subtract(matrix2);
+        return newMatrix;
+    }
+
+    //============================================================================================
 }
 
