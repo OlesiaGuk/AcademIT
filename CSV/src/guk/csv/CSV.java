103015c020a2470a993a1d8149775191b0e1d30a
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-10-09 21:00:01 +0700

    [CSV] корректировка по замечаниям(1)

--- CSV/src/guk/csv/CSV.java
+++ CSV/src/guk/csv/CSV.java
@@ -1,93 +1,65 @@
 package guk.csv;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CSV {
-    public static void main(String[] args) throws IOException {
-        String fileName = "CSV/src/guk/csv/input.csv";
-        List<Character> charsList = readToList(fileName);
-
-        List<List<String>> lines = splitText(charsList);
-
-        printToHtml(lines);
-    }
-
-    private static List<Character> readToList(String fileName) throws IOException {
-        List<Character> charsList = new ArrayList<>();
-
-        try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
-
-            int ch;
-            while ((ch = in.read()) != -1) {
-                charsList.add((char) ch);
-            }
-        }
-        return charsList;
-    }
-
-    private static List<List<String>> splitText(List<Character> charsList) {
-        List<List<String>> lines = new ArrayList<>();
-        boolean hasQuotes = false;
-        StringBuilder s = new StringBuilder();
-
-        s.append("<tr>").append("<td>");
-        for (int k = 0; k < charsList.size(); k++) {
-            if (charsList.get(k) == '"') {
-                if (charsList.get(k + 1) != '"') {
-                    hasQuotes = !hasQuotes;
-                    continue;
-                } else if (charsList.get(k + 1) == '"' && charsList.get(k + 2) == '"') {
-                    s.append(charsList.get(k));
-                    k += 3;
-                    continue;
-                } else {
-                    s.append(charsList.get(k));
-                    k++;
-                    continue;
-                }
-            }
-
-            if (charsList.get(k) == '\r') {
-                if (!hasQuotes) {
-                    List<String> cell = new ArrayList<>();
-                    cell.add(s.append("</td>").append("</tr>").toString());
-                    lines.add(cell);
-                    k++;
-                    s.setLength(0);
-                    s.append("<tr>").append("<td>");
-                    continue;
-                } else {
-                    s.append("<br/>");
-                    k++;
-                    continue;
-                }
-            }
-
-            if (charsList.get(k) == ',' && !hasQuotes) {
-                List<String> cell = new ArrayList<>();
-                cell.add(s.append("</td>").toString());
-                lines.add(cell);
-                s.setLength(0);
-                s.append("<td>");
-                continue;
-            }
-            s.append(charsList.get(k));
-        }
-        return lines;
-    }
-
-    private static void printToHtml(List<List<String>> lines) throws IOException {
-        try (PrintWriter out = new PrintWriter("CSV/src/guk/csv/output.html")) {
+    public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new FileReader(args[0]));
+             PrintWriter out = new PrintWriter(args[1])) {
+            out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
             out.println("<head><meta charset=\"utf-8\"></head>");
+            out.println("<body>");
             out.println("<table border=\"1\">");
 
-            for (List<String> line : lines) {
-                line.forEach(out::println);
+            String line;
+            boolean hasQuotes = false;
+
+            while ((line = in.readLine()) != null) {
+                if (!hasQuotes) {
+                    out.print("<tr><td>");
+                }
+
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '"') {
+                        if (line.charAt(i + 1) != '"') {
+                            hasQuotes = !hasQuotes;
+                        } else if (line.charAt(i + 1) == '"' && line.charAt(i + 2) == '"') {
+                            out.print(line.charAt(i));
+                            i += 3;
+                        } else {
+                            out.print(line.charAt(i));
+                            i++;
+                        }
+
+                    } else if (line.charAt(i) == ',' && !hasQuotes) {
+                        out.println("</td>");
+                        out.print("<td>");
+
+                    } else if (line.charAt(i) == '<') {
+                        out.print("&lt");
+                    } else if (line.charAt(i) == '>') {
+                        out.print("&gt");
+                    } else if (line.charAt(i) == '&') {
+                        out.print("&amp");
+
+                    } else {
+                        out.print(line.charAt(i));
+                    }
+                }
+
+                if (hasQuotes) {
+                    out.print("<br/>");
+                } else {
+                    out.println("</td></tr>");
+                }
             }
 
-            out.print("</table>");
+            out.println("</table>");
+            out.println("</body>");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ошибка при работе с файлом: " + e.getMessage());
         }
     }
 }
