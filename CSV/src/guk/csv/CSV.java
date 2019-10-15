8794e4a9f5af2101d8a4077c4b3b10607e8f7317
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-10-15 14:20:40 +0700

    [CSV] корректировка по замечаниям(3)

--- CSV/src/guk/csv/CSV.java
+++ CSV/src/guk/csv/CSV.java
@@ -7,63 +7,63 @@ public class CSV {
         if (args.length != 2) {
             System.out.println("Неверное количество аргументов программы!");
             System.out.println("Программе необходимо задать 2 аргумента: путь к исходному CSV-файлу и путь к итоговому HTML-файлу");
-        } else {
-            try (BufferedReader in = new BufferedReader(new FileReader(args[0]));
-                 PrintWriter out = new PrintWriter(args[1])) {
-                out.println("<!DOCTYPE html>");
-                out.println("<head><meta charset=\"utf-8\"></head>");
-                out.println("<body>");
-                out.println("<table border=\"1\">");
+            return;
+        }
 
-                String line;
-                boolean hasQuotes = false;
+        try (BufferedReader in = new BufferedReader(new FileReader(args[0]));
+             PrintWriter out = new PrintWriter(args[1])) {
+            out.println("<!DOCTYPE html>");
+            out.println("<head><meta charset=\"utf-8\"></head>");
+            out.println("<body>");
+            out.println("<table border=\"1\">");
 
-                while ((line = in.readLine()) != null) {
-                    if (!hasQuotes) {
-                        out.print("<tr><td>");
-                    }
+            String line;
+            boolean hasQuotes = false;
 
-                    for (int i = 0; i < line.length(); i++) {
-                        if (line.charAt(i) == '"') {
-                            if ((i + 1) == line.length() || line.charAt(i + 1) != '"') {
-                                hasQuotes = !hasQuotes;
-                            } else if (line.charAt(i + 1) == '"' && line.charAt(i + 2) == '"') {
-                                out.print(line.charAt(i));
-                                i += 3;
-                            } else {
-                                out.print(line.charAt(i));
-                                i++;
-                            }
+            while ((line = in.readLine()) != null) {
+                if (!hasQuotes) {
+                    out.print("<tr><td>");
+                }
 
-                        } else if (line.charAt(i) == ',' && !hasQuotes) {
-                            out.println("</td>");
-                            out.print("<td>");
-                        } else if (line.charAt(i) == '<') {
-                            out.print("&lt;");
-                        } else if (line.charAt(i) == '>') {
-                            out.print("&gt;");
-                        } else if (line.charAt(i) == '&') {
-                            out.print("&amp;");
-
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '"') {
+                        if ((i + 1) == line.length() || line.charAt(i + 1) != '"') {
+                            hasQuotes = !hasQuotes;
+                        } else if (line.charAt(i + 1) == '"' && line.charAt(i + 2) == '"') {
+                            out.print(line.charAt(i));
+                            i += 3;
                         } else {
                             out.print(line.charAt(i));
+                            i++;
                         }
-                    }
 
-                    if (hasQuotes) {
-                        out.print("<br/>");
+                    } else if (line.charAt(i) == ',' && !hasQuotes) {
+                        out.println("</td>");
+                        out.print("<td>");
+                    } else if (line.charAt(i) == '<') {
+                        out.print("&lt;");
+                    } else if (line.charAt(i) == '>') {
+                        out.print("&gt;");
+                    } else if (line.charAt(i) == '&') {
+                        out.print("&amp;");
                     } else {
-                        out.println("</td></tr>");
+                        out.print(line.charAt(i));
                     }
                 }
 
-                out.println("</table>");
-                out.println("</body>");
-            } catch (FileNotFoundException e) {
-                System.out.println("Файл не найден: " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Ошибка при работе с файлом! " + e.getClass() + ": " + e.getMessage());
+                if (hasQuotes) {
+                    out.print("<br/>");
+                } else {
+                    out.println("</td></tr>");
+                }
             }
+
+            out.println("</table>");
+            out.println("</body>");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ошибка при работе с файлом! " + e.getClass() + ": " + e.getMessage());
         }
     }
 }
